9d0a10ba43c47b5ecb3b5657593bec111b71e382
Derouiche <cobwild_child@hotmail.com>
2020-05-28 18:20:50 +0100

    Initial Commit

---
+++ algo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  algo
+//
+//  Created by 360medlink Tunisia on 5/28/20.
+//  Copyright © 2020 360medlink Tunisia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
